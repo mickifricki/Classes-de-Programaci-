6844ed2baa7d44e02ccc90a50789b3593e4b9275
Miquel Sans Canudas <mickie.snas.11@gmail.com>
2015-03-03 12:11:37 +0100

    Clase String Acabada (2)

--- Cadena/Cadena/UnitTest1/TestClaseString.cpp
+++ Cadena/Cadena/UnitTest1/TestClaseString.cpp
@@ -19,20 +19,20 @@ namespace UnitTest1
 			Assert::AreEqual(string1.Length(), 0);
 		}
 
-		TEST_METHOD(testStringChar)
+		TEST_METHOD(testStringCh)
 		{
-			String string1("hola");
+			String string1("high");
 
-			Assert::AreEqual(string1.GetString(), "hola");
+			Assert::AreEqual(string1.GetString(), "high");
 		}
 
 
-		TEST_METHOD(testStringCharSame)
+		TEST_METHOD(testStringChEqual)
 		{
 
 
-			String string1("hola");
-			String string2("hola");
+			String string1("high");
+			String string2("high");
 
 			Assert::IsTrue(string1.operator == (string2));
 
@@ -40,24 +40,24 @@ namespace UnitTest1
 
 		TEST_METHOD(testStringCharNotSame)
 		{
-			String string1("hola");
-			String string2("aloh");
+			String string1("high");
+			String string2("hgih");
 
 			Assert::IsTrue(string1.operator != (string2));
 		}
 
-		TEST_METHOD(testStringRefSame)
+		TEST_METHOD(testStringRefEqual)
 		{
-			String string1("hola");
-			String string2("hola");
+			String string1("high");
+			String string2("high");
 
 			Assert::IsTrue(string1.operator == (string2));
 		}
 
-		TEST_METHOD(testStringRefNotSame)
+		TEST_METHOD(testStringRefNotEqual)
 		{
-			String string1("hola");
-			String string2("aloh");
+			String string1("high");
+			String string2("hgih");
 
 			Assert::IsTrue(string1.operator != (string2));
 		}
@@ -65,7 +65,7 @@ namespace UnitTest1
 
 		TEST_METHOD(testStringLength)
 		{
-			String string1("hola");
+			String string1("high");
 			string1.Length();
 
 			Assert::AreEqual(string1.Length(), 4);
@@ -75,22 +75,22 @@ namespace UnitTest1
 
 		TEST_METHOD(testStringCapacity)
 		{
-			String string1("hola");
+			String string1("high");
 			string1.Capacity();
 			Assert::AreEqual(string1.Capacity(), 4);
 		}
 
 		TEST_METHOD(testStringClear)
 		{
-			String string1("hola");
+			String string1("high");
 			string1.Clear();
 			Assert::AreEqual(string1.Length(), 0);
-			//Assert::IsTrue(strcmp(string1.GetString(),"") == 0);
+			
 		}
 		TEST_METHOD(testGetString)
 		{
-			String string1("hola");
-			Assert::AreEqual(string1.GetString(), "hola");
+			String string1("high");
+			Assert::AreEqual(string1.GetString(), "high");
 		}
 	};
 }
