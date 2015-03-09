a10198e63c70ab46e047b4e32ffa493430935613
Miquel Sans Canudas <mickie.snas.11@gmail.com>
2015-03-09 12:13:50 +0100

    Modificació

---
+++ Clase 06-03-2015/Clase 06-03-2015/ClassList.h
@@ -0,0 +1,46 @@
+#ifndef _ClassList_H_
+#define _ClassList_H_
+
+#include <stdio.h>
+
+class SList {
+
+	struct node {
+		int valor;
+		node* next;
+	};
+
+	node* start;
+	SList():start(NULL){};
+	void Add(int valor) {
+		node* new_node = new node;
+		new_node->valor = valor;
+		new_node->next = NULL;
+		node* tmp = start;
+		if (tmp != NULL){
+			while (tmp->next != NULL){
+				tmp = tmp->next;
+			}
+		}
+		tmp->next = new_node;
+	};
+	
+	unsigned int Count() const{
+		unsigned int resultat = 0;
+		node* tmp = start;
+		while (tmp != NULL){
+			tmp = tmp->next;
+			resultat++;
+		}
+		return resultat;
+	};
+
+	void Del(node* n){
+
+	}
+	
+
+
+};
+
+#endif //_ClassList_H_
