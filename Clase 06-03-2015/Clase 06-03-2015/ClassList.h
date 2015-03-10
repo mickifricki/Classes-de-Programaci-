370f98ce07bb2a57e130408ee6efca77fad3ae14
Miquel Sans Canudas <mickie.snas.11@gmail.com>
2015-03-10 11:58:33 +0100

    funció delete

--- Clase 06-03-2015/Clase 06-03-2015/ClassList.h
+++ Clase 06-03-2015/Clase 06-03-2015/ClassList.h
@@ -3,30 +3,57 @@
 
 #include <stdio.h>
 
+
+//https://ronnyml.wordpress.com/2009/07/04/listas-enlazadas-clase-lista-en-c/
+
 class SList {
 
 	struct node {
 		int valor;
 		node* next;
+		void DeleteAll(){
+			if (next)
+				next->DeleteAll();
+			delete this;
+		}
 	};
 
-private:
+//private:
 	node* start;
-public:
+//public:
 
 	
 	SList():start(NULL){};
-	void Add(int valor) {
+	void AddLast(int valor) {
 		node* new_node = new node;
-		new_node->valor = valor; 
+		new_node->valor = valor;
 		new_node->next = NULL;
 		node* tmp = start;
-		if (tmp != NULL){
+		if (!start){
+			start = new_node;
+		}
+		else{
 			while (tmp->next != NULL){
 				tmp = tmp->next;
 			}
+			tmp->next = new_node;
 		}
-		tmp->next = new_node;
+	};
+	void AddStart(int valor) {
+		node* new_node = new node;
+		new_node->valor = valor;
+		new_node->next = NULL;
+		node* tmp = start;
+		if (!start){
+			start = new_node;
+		}
+		else{
+			while (tmp->next != NULL){
+				tmp = tmp->next;
+			}
+			new_node->next = start;
+			start = new_node;
+		}
 	};
 	
 	unsigned int Count() const{
@@ -39,11 +66,32 @@ public:
 		return resultat;
 	};
 
-	void Del(node* n){
-		
+	void DelAll(){
+		start->DeleteAll();
+		start = 0;
+	}
 
+	void DelNodePos(int pos){
+		node *tmp = start;
+		node *tmp1 = tmp->next;
 
-
+		if (pos < 1 || pos > Count()){
+			printf("Fuera de Rango capullo!");
+		}
+		else if (pos == 1){
+			start = tmp->next;
+		}
+		else {
+			for (int i = 2; i <= pos; i++){
+				if (i == pos){
+					//node *auxiliar_node = tmp1;
+					tmp->next = tmp1->next;
+					//delete auxiliar_node;
+				}
+				tmp = tmp->next;
+				tmp1 = tmp1->next;
+			}
+		}
 	}
 	
 
--- Clase 06-03-2015/UnitTest1/unittest1.cpp
+++ Clase 06-03-2015/UnitTest1/unittest1.cpp
@@ -15,9 +15,14 @@ namespace UnitTest1
 			// TODO: Agregar aquí el código de la prueba
 			SList Lista;
 			int a;
-			Lista.Add(-2);
+			Lista.AddLast(2);
+			Lista.AddLast(2);
+			Lista.AddLast(2);
+			Lista.AddStart(6);
+			Lista.AddLast(2);
+			Lista.DelNodePos(3);
 			a = Lista.Count();
-			Assert::AreEqual(a, 5);
+			Assert::AreEqual(a, 4);
 
 
 		}
