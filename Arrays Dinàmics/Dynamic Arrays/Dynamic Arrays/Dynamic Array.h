a02ac8f506097d677d65ded2bba5633998cfa903
mickifricki <mickie.snas.11@gmail.com>
2015-03-17 14:04:21 +0100

    Arrays dinámics. Noves funcions

--- Arrays Dinàmics/Dynamic Arrays/Dynamic Arrays/Dynamic Array.h
+++ Arrays Dinàmics/Dynamic Arrays/Dynamic Arrays/Dynamic Array.h
@@ -2,6 +2,7 @@
 #define _Dynamic_Array_H_
 
 #include <stdio.h>
+#include <assert.h>
 
 class DynamicArray
 {
@@ -11,13 +12,69 @@ private:
 	unsigned int uinumber_elements;
 
 public: 
-	DynamicArray() :idata(NULL), uiallocated_memory(0), uinumber_elements(0){}
-	~DynamicArray(){ if (idata != NULL) delete[] idata; }
-	void Reallocate(unsigned int new_memory_size); 
-	void PushBack(int value);
-	int Pop();
+	DynamicArray() :idata(NULL), uiallocated_memory(0), uinumber_elements(0){};
+	~DynamicArray(){ if (idata != NULL) delete[] idata; };
+	void Reallocate(unsigned int uinew_memory_size)
+	{
+		assert(uinew_memory_size > 0);
+		delete[] idata;
+		idata = new int[uinew_memory_size];
+		uiallocated_memory = uinew_memory_size;
+		uinumber_elements = uinew_memory_size;
+	};
+	void CopyArray(int *isource, int* idestination)
+	{
+		for (int i = 0; i < uinumber_elements; i++)
+		{
+			idestination[i] = isource[i];
+		}
+	};
+	void PushBack(int value)
+	{
+		if (uinumber_elements){
+			if (uiallocated_memory - uinumber_elements == 0)
+			{
+				int* tmp = new int[uinumber_elements];
+				CopyArray(idata, tmp);
+				Reallocate(uiallocated_memory + 1);
+				CopyArray(tmp, idata);
+				idata[uinumber_elements - 1] = value;
+			}
+			else if (uiallocated_memory - uinumber_elements > 0)
+			{
+				idata[uinumber_elements] = value;
+			}
+			else
+				printf("Fuera de rango jeuputa!");
+		else
+		{
+			Reallocate(1);
+			idata[uinumber_elements - 1] = value;
+		}
+	};
+	int Pop()
+	{
+		int value = 0;
+		if (uinumber_elements > 0)
+		{
+			value = idata[uinumber_elements];
+			--uinumber_elements;
+
+			return value;
+		}
+		else
+			return NULL;
+	}
 	void Insert(int value, unsigned int position);
-	int& operator[] (unsigned int index); //torna una posició, retorna idata. Modifica
-	const int& operator[] (unsigned int index) const; //Llegeix una posició, però no modifica res.
+	int& operator[] (unsigned int index)
+	{
+		assert(index < uinumber_elements);
+			return idata[index];
+	} //returns a position, returns idata. Modifies.
+	const int& operator[] (unsigned int index) const
+	{
+		assert(index < uinumber_elements);
+		return idata[index];
+	} //Reads a position, but not modifies its value.
 };
 #endif
